765a3c08d9bb60a41568ebd732cd1f8158310bbf
Benjamin Webb <bwebb@lincolninst.edu>
2023-08-08 13:25:08 -0400

    Update util.py

--- nldi/util.py
+++ nldi/util.py
@@ -31,8 +31,10 @@
 
 import json
 import logging
+import os
 from pathlib import Path
-from typing import IO
+import re
+from typing import IO, Union
 import yaml
 
 LOGGER = logging.getLogger(__name__)
@@ -67,7 +69,46 @@ def yaml_load(fh: IO) -> dict:
     :returns: `dict` representation of YAML
     """
 
-    return yaml.load(fh, Loader=yaml.SafeLoader)
+    # support environment variables in config
+    # https://stackoverflow.com/a/55301129
+    path_matcher = re.compile(r'.*\$\{([^}^{]+)\}.*')
+
+    def path_constructor(loader, node):
+        env_var = path_matcher.match(node.value).group(1)
+        if env_var not in os.environ:
+            msg = f'Undefined environment variable {env_var} in config'
+            raise EnvironmentError(msg)
+        return get_typed_value(os.path.expandvars(node.value))
+
+    class EnvVarLoader(yaml.SafeLoader):
+        pass
+
+    EnvVarLoader.add_implicit_resolver('!path', path_matcher, None)
+    EnvVarLoader.add_constructor('!path', path_constructor)
+
+    return yaml.load(fh, Loader=EnvVarLoader)
+
+
+def get_typed_value(value: str) -> Union[float, int, str]:
+    """
+    Derive true type from data value
+
+    :param value: value
+
+    :returns: value as a native Python data type
+    """
+
+    try:
+        if '.' in value:  # float?
+            value2 = float(value)
+        elif len(value) > 1 and value.startswith('0'):
+            value2 = value
+        else:  # int?
+            value2 = int(value)
+    except ValueError:  # string (default)?
+        value2 = value
+
+    return value2
 
 
 def to_json(dict_: dict, pretty: bool = False) -> str:
