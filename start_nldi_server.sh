e91a9d06dcd0d1ed470e30187671403be1d97467
gtrantham <gtrantham@usgs.gov>
2024-11-12 14:15:06 -0600

    change worker count

--- start_nldi_server.sh
+++ start_nldi_server.sh
@@ -7,17 +7,14 @@ set +e
 
 export NLDI_HOME=/nldi
 export NLDI_CONFIG="${NLDI_HOME}/local.source.yml"
-# export NLDI_OPENAPI="${NLDI_HOME}/local.openapi.yml"
 
-# export PYGEOAPI_CONFIG="${NLDI_HOME}/pygeoapi.config.yml"
-# export PYGEOAPI_OPENAPI="${NLDI_HOME}/pygeoapi.openapi.yml"
 
 # gunicorn env settings with defaults
 #export SCRIPT_NAME=${NLDI_PATH:=/}
 CONTAINER_NAME=${CONTAINER_NAME:=nldi}
 CONTAINER_HOST=${CONTAINER_HOST:=0.0.0.0}
 CONTAINER_PORT=${CONTAINER_PORT:=8081}
-WSGI_WORKERS=${WSGI_WORKERS:=1}
+WSGI_WORKERS=${WSGI_WORKERS:=4}
 WSGI_WORKER_TIMEOUT=${WSGI_WORKER_TIMEOUT:=6000}
 WSGI_WORKER_CLASS=${WSGI_WORKER_CLASS:=gevent}
 
