84a11b45a38ebda2ec62a9798ce69b027871e2de
gtrantham <gtrantham@usgs.gov>
2025-05-21 13:42:05 -0500

    HOTFIX #84

--- src/nldi/server/linked_data/services/feature.py
+++ src/nldi/server/linked_data/services/feature.py
@@ -106,7 +106,7 @@ class FeatureService(FlaskServiceMixin, SQLAlchemySyncRepositoryService[FeatureS
         hits = self.repository._execute(stmt)
         r = hits.fetchall()
         # NOTE: we're returning a generator comprehension here -- so this function is used as an iterator.
-        return (f[0].as_feature(excl_props=["reachcode"]) for f in r)
+        return (f[0].as_feature() for f in r)
 
 
 def feature_svc(db_session: Session) -> Generator[FeatureSourceModel, None, None]:
