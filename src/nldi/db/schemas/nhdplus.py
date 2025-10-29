9d465ae02707f07799b136493ac0c04d16cdb24a
Benjamin Webb <benjamin.miller.webb@gmail.com>
2025-10-29 11:20:33 -0600

    Fix grammar

--- src/nldi/db/schemas/nhdplus.py
+++ src/nldi/db/schemas/nhdplus.py
@@ -66,8 +66,7 @@ class FlowlineModel(NHDBaseModel, GeoJSONMixin):
 
     def __properties__(self, exclude: set) -> dict[str, str]:
         _props = super().__properties__(exclude)
-        ## Extend the properties dict with the AssociationProxy(s) of iterest; these are not dumped with columns by default.
-        ## Extend the properties dict with the AssociationProxy(s) of iterest; these are not dumped with columns by default.
+        ## Extend the properties dict with the AssociationProxy(s) of interest; these are not dumped with columns by default.
         if self.mainstem == "NA" or self.mainstem == "":  # possible strings to interpret as NoData for the mainstem URI.
             _mainstem = None
         else:
