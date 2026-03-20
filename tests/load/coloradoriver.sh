e6a106a2511bd0841e5a8bba63e5c4340977e045
gtrantham <gtrantham@usgs.gov>
2026-03-20 09:53:01 -0500

    bump version

--- tests/load/coloradoriver.sh
+++ tests/load/coloradoriver.sh
@@ -6,8 +6,9 @@ START_COMID=21412883  ## This comid selected from https://reference.geoconnex.us
 N=5                 ## Number of jobs in a batch.  These jobs run serially.
 PARALLELISM=9        ## How many batches to launch.  Batches run in parallel... the jobs within are serial.
 
-#BASE_URL="https://labs-beta.waterdata.usgs.gov/api/nldi"  # Use for QA
-BASE_URL="https://nhgf.dev-wma.chs.usgs.gov/api/nldi-py"  # Use for internal dev
+# nhgf-beta.wma.chs.usgs.gov
+BASE_URL="https://labs-beta.waterdata.usgs.gov/api/nldi"  # Use for QA
+#BASE_URL="https://nhgf.dev-wma.chs.usgs.gov/api/nldi-py"  # Use for internal dev
 echo  "Fetching a bunch of COMIDs to work on...  "
 #echo "${BASE_URL}/linked-data/comid/${START_COMID}/navigation/UM/flowlines?distance=1000&f=json&excludeGeometry=true"
 #curl -s "${BASE_URL}/linked-data/comid/${START_COMID}/navigation/UM/flowlines?distance=1000&f=json&excludeGeometry=true" | jq '.features.[].properties.nhdplus_comid' > ./worklist.txt
