ae25d65692e25e19af098e5a1d04d0f30b063f8f
gtrantham <gtrantham@usgs.gov>
2026-03-30 18:49:35 -0500

    Clean slate: remove all application code, strip to minimal deps
    Remove all existing source under src/nldi/ except __init__.py.
    Strip pyproject.toml to litestar + uvicorn only.
    All other dependencies added back when code needs them.
    Version bumped to 3.0.0-dev.

--- src/nldi/db/schemas/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-# SPDX-License-Identifier: CC0-1.0
-# SPDX-FileCopyrightText: 2024-present USGS
-# See the full copyright notice in LICENSE.md
-#
-
-"""Module containing NLDI schema models"""
